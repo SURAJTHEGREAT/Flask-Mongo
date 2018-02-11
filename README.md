12dacd1fe201d0cfefc43e79f75226a732ddfb7b
Suraj S <suraj.s@cognizant.com>
2018-02-11 21:43:47 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# Flask-Mongo
+----USER DOCKER COMPOSE FILE----
+
+1. docker-compose up -d -(brings all the containers - mongo and python)
---
+++ python_apps/test-script.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from pymongo import MongoClient
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    
+    client = MongoClient()
+    if client:
+         print 'Client connected'
+    return 'Flask Dockerized'      
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
